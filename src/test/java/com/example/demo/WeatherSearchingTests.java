f2291ced13131fc78973674574a823e3ba75c7d5
unknown <kaisellin@gmail.com>
2022-07-12 21:16:11 +0300

    Changes to location weather finding

--- src/test/java/com/example/demo/WeatherSearchingTests.java
+++ src/test/java/com/example/demo/WeatherSearchingTests.java
@@ -2,9 +2,14 @@ package com.example.demo;
 
 import com.codeborne.selenide.selector.ByTagAndText;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class WeatherSearchingTests {
@@ -20,9 +25,18 @@ public class WeatherSearchingTests {
 
         new LocationWeather().locationWeatherSearch("Tartu");
 
-        WeatherResultsPage results = new WeatherResultsPage();
-        results.getResults().shouldHave(sizeGreaterThan(0));
-        results.getResult(0).shouldHave(text("Weather History for Tartu"));
+        $(withText("Tartu")).shouldBe(visible);
+
+    }
+
+    @Test
+    public void getWeatherForecast() {
+        String apiKey = "BMU4JN68Q5EN7B6ZPQBPKCFV2";
+        String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Tallinn?unitGroup=metric&key=__API_KEY__&contentType=json".replace("_API_KEY_",apiKey);
+
+        ResponseEntity<String> response = new TestRestTemplate().getForEntity(url, String.class);
+        System.out.println(response.getStatusCodeValue());
+        System.out.println(response.getBody());
     }
 
 }
