181cf95e9b7ce29b77ba2536753058f59086b5ff
unknown <kaisellin@gmail.com>
2022-07-13 11:06:36 +0300

    adding API test and fixing smaller issues

--- src/test/java/com/example/demo/WeatherSearchingTests.java
+++ src/test/java/com/example/demo/WeatherSearchingTests.java
@@ -2,16 +2,13 @@ package com.example.demo;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.selector.ByTagAndText;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class WeatherSearchingTests {
@@ -45,13 +42,18 @@ public class WeatherSearchingTests {
     }
 
     @Test
-    public void getWeatherForecast() {
+    public void getWeatherForecast() throws JSONException {
         String apiKey = "BMU4JN68Q5EN7B6ZPQBPKCFV2";
-        String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Tallinn?unitGroup=metric&key=__API_KEY__&contentType=json".replace("_API_KEY_",apiKey);
+        String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Tallinn?unitGroup=metric&key=_API_KEY_&contentType=json".replace("_API_KEY_",apiKey);
 
         ResponseEntity<String> response = new TestRestTemplate().getForEntity(url, String.class);
+
         System.out.println(response.getStatusCodeValue());
         System.out.println(response.getBody());
+
+        JSONObject json = new JSONObject(response.getBody());
+        Assertions.assertEquals(json.getString("address"), "Tallinn");
+
     }
 
 }
